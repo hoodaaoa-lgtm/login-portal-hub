33ed41da05aae0b0dc789f3d13ce14fd22529216
Hooda <hooda.aoa@gmail.com>
2026-07-04 10:44:27 +0000

    Remove scroll interno da sidebar direita, fica totalmente fixa em todas as paginas

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -41,7 +41,7 @@ export function RightSidebar() {
   }
 
   return (
-    <aside className="hidden xl:flex flex-col fixed right-0 top-0 bottom-0 w-[360px] z-30 pl-8 pr-6 py-3 overflow-y-auto"
+    <aside className="hidden xl:flex flex-col fixed right-0 top-0 bottom-0 w-[360px] z-30 pl-8 pr-6 py-3 overflow-hidden"
       style={{ background: "var(--surface-0)" }}>
       <div className="w-full space-y-4">
 
